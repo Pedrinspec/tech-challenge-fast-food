65f419e1b2daffa5b1ea33826800228e50909596
Pedrinspec <pedrinho.top07@gmail.com>
2025-04-09 21:41:18 -0300

    First Commit

---
+++ src/main/java/com/fiap/fast_food_tc/FastFoodTcApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.fast_food_tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodTcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodTcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/fast_food_tc/FastFoodTcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.fast_food_tc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodTcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
