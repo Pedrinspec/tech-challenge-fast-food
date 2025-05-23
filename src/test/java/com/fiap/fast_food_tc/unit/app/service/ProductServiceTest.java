e05ecde9b949ad4a522052ca91b3a2c5c60e299c
gustavohaj <gustavohenriqueaj@gmail.com>
2025-05-23 14:07:42 -0300

    Create unit tests findAll method product service.

--- src/test/java/com/fiap/fast_food_tc/unit/app/service/ProductServiceTest.java
+++ src/test/java/com/fiap/fast_food_tc/unit/app/service/ProductServiceTest.java
@@ -1,31 +1,46 @@
 package com.fiap.fast_food_tc.unit.app.service;
 
-import com.fiap.fast_food_tc.adapter.provider.ProductDataProvider;
+import com.fiap.fast_food_tc.app.service.impl.ProductServiceImpl;
+import com.fiap.fast_food_tc.cross.mapper.ProductMapper;
+import com.fiap.fast_food_tc.domain.usecase.ProductUseCase;
 import fixture.ProductFixture;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static fixture.ProductFixture.createProductResponse;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
 
-
+@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
 
-    private ProductDataProvider productService;
+    @Mock
+    private ProductMapper productMapper;
+
+    @Mock
+    private ProductUseCase productUseCase;
+
+    @InjectMocks
+    private ProductServiceImpl productServiceImpl;
 
     @Test
-    void findAllReturnMappedProductResponseList(){
-        // Arrange
-        var product = ProductFixture.createProduct();
-        var productResponse = ProductFixture.createProduct();
-       // when(productUseCase.findAll()).thenReturn(List.of(product));
-      //  when(productMapper.toResponseList(List.of(product))).thenReturn(List.of(productResponse));
+    void findAllSuccessTest(){
+        //mockar tudo que é necessário injetar
+        Mockito.when(productMapper.toResponseList(any())).thenReturn(List.of(createProductResponse()));
+        Mockito.when(productUseCase.findAll()).thenReturn(List.of(ProductFixture.createEProduct()));
 
-        // Act
-        var result = productService.findAll();
+        //chamar o método da classe a ser testada
+        var response = productServiceImpl.findAll();
 
-        // Assert
-        assertEquals(1, result.size());
-        assertEquals(productResponse, result.get(0));
+        //verificar se o retorno é o esperado
+        assertNotNull(response);
+        assertEquals(1, response.size());
     }
 
 }
--- src/test/java/fixture/ProductFixture.java
+++ src/test/java/fixture/ProductFixture.java
@@ -1,7 +1,7 @@
 package fixture;
 
 import com.fiap.fast_food_tc.adapter.db.model.Product;
-import com.fiap.fast_food_tc.cross.mapper.ProductMapper;
+import com.fiap.fast_food_tc.adapter.dto.product.ProductResponse;
 import com.fiap.fast_food_tc.domain.entity.EProduct;
 import com.fiap.fast_food_tc.domain.usecase.ProductUseCase;
 
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 
 public class ProductFixture {
 
-    private static ProductUseCase ProductUseCase;
+
 
     public static Product createProduct() {
         return Product.builder()
@@ -29,5 +29,14 @@ public class ProductFixture {
                 .build();
     }
 
+
+    public static ProductResponse createProductResponse() {
+        return ProductResponse.builder()
+                .productId(1)
+                .name("Hamburguer")
+                .description("Delicious hamburguer")
+                .productValue(BigDecimal.ONE)
+                .build();
+    }
 
 }
