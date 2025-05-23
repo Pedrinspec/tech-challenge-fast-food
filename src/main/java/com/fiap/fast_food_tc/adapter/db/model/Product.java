253d65166366190981b23386ee43df0063d2d066
gustavohaj <gustavohenriqueaj@gmail.com>
2025-05-23 14:23:33 -0300

    troca do modificador de acesso do campo productId

--- src/main/java/com/fiap/fast_food_tc/adapter/db/model/Product.java
+++ src/main/java/com/fiap/fast_food_tc/adapter/db/model/Product.java
@@ -31,7 +31,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id", nullable = false, unique = true)
-    public Integer productId;
+    private Integer productId;
 
     @Column(name = "name", nullable = false)
     private String name;
