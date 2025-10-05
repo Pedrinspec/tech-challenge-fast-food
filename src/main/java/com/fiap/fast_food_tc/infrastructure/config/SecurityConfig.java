95dd99628794dfe926ff082690bf77537560a16f
Leonardo Fujimura <leonardo.fujimura123@gmail.com>
2025-10-05 01:06:51 -0300

    Adiciona necessidade de token para realizar requisições

---
+++ src/main/java/com/fiap/fast_food_tc/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.fiap.fast_food_tc.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> {})
+                );
+
+        return http.build();
+    }
+}
