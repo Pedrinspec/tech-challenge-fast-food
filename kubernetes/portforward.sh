77e1a3b01a4ed3e32c02e4777e567e8ade53ecf1
gustavohaj <seu-email@github.com>
2025-10-05 20:53:50 -0300

    removendo os manifestos k8s

--- kubernetes/portforward.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-target_port=$(kubectl get svc fastfood-app -n desafio-fiap -o jsonpath='{.spec.ports[0].port}')
-pod=$(kubectl get pods -n desafio-fiap -l app=fastfood-app -o jsonpath='{.items[0].metadata.name}')
-
-echo "Port forwarding localhost:8080 -> $pod:$target_port"
-echo "Port forwarding established. Access the application at http://localhost:8080/swagger-ui.html"
-kubectl port-forward -n desafio-fiap $pod 8080:$target_port
